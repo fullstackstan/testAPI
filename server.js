54a1ed48e7205c9218cd327413b5cc31b4dfe858
fullstackstan <fullstackstan@gmail.com>
2022-10-04 12:05:24 -0500

    fin

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const app = express()
+
+require('dotenv').config({path: '.env'})
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('hello')
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running${process.env.PORT}, you better catch it!`)
+})   
